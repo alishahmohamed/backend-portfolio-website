9ec5e3ca80855edb30675fed733975e9e8963eba
+ <+>
2023-03-06 17:32:21 +0300

    commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,12 +20,12 @@ class ApplicationController < Sinatra::Base
 
   # a user should be able to view all their listed projects
   get '/projects/:id' do
-    project = User.find(params[:id]).projects
+    project = User.find(params[:id]).projects  
     project.to_json
   end
 
   # # A user should be able to add a new project to their portfolio.
-  post '/create-project/:id' do
+  post '/create-project/:id' do  
     project = User.find(params[:id]).projects.create(
       title: params[:title],
       description: params[:description],
